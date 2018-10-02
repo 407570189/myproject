21dffc111738fec11ef3438d980b1fff216234b4
user <user@es302n03>
2018-10-02 10:25:33 +0800

    first

---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,21 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//Hello
+		int age= 19;
+        String firstname ="Hung";
+        String lastname = "Shang";
+        String message = "hello";
+        float weight =(float)58.5;
+        float tall = 176.5f;
+		System.out.println(message);
+		System.out.println("my name is");
+        System.out.println(firstname+lastname);
+        System.out.println(age+"years old");
+		System.out.println("tall"+tall);
+		System.out.println("weight"+weight);
+	}
+
+}
