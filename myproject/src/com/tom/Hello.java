68a279727cee41119ca30c782a0a77843d281b76
user <user@es302n03>
2018-10-02 10:44:43 +0800

    second

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -3,18 +3,18 @@ package com.tom;
 public class Hello {
 
 	public static void main(String[] args) {
-		//Hello
+		//Hello introduce myself
 		int age= 19;
         String firstname ="Hung";
         String lastname = "Shang";
         String message = "hello";
         float weight =(float)58.5;
-        float tall = 176.5f;
+        float height = 176.5f;
 		System.out.println(message);
 		System.out.println("my name is");
         System.out.println(firstname+lastname);
         System.out.println(age+"years old");
-		System.out.println("tall"+tall);
+		System.out.println("tall"+height);
 		System.out.println("weight"+weight);
 	}
 
