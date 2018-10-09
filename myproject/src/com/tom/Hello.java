a39c0d18937a5307fd0aba539ac48ba937eb9236
user <user@es302n03>
2018-10-09 12:00:44 +0800

    third

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -4,18 +4,33 @@ public class Hello {
 
 	public static void main(String[] args) {
 		//Hello introduce myself
-		int age= 19;
+		Person p = new Person(58.5f,1.765f);
+		p.weight = 58.5f;
+		p.height = 1.765f;
+		
+		System.out.println(p.bmi());	
+		p.hello(); 
+		//Person hung = new Person();
+		
+		/*int age= 19;
         String firstname ="Hung";
         String lastname = "Shang";
         String message = "hello";
         float weight =(float)58.5;
         float height = 176.5f;
+        float bmi = weight / (height*height);
 		System.out.println(message);
 		System.out.println("my name is");
         System.out.println(firstname+lastname);
         System.out.println(age+"years old");
 		System.out.println("tall"+height);
 		System.out.println("weight"+weight);
+		System.out.println(bmi);
+		age = age + 1;
+		System.out.println(age);
+		    */
+	       
+	       
 	}
 
 }
